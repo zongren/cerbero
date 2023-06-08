5ff9a3fd26c8a588f5f3fc6a3d9b523a179b6879
宗仁 <zongren@live.com>
2023-06-08 16:00:29 +0800

    Upgrade NDK to r25c

--- cerbero/bootstrap/android.py
+++ cerbero/bootstrap/android.py
@@ -25,10 +25,11 @@ from cerbero.config import Distro, FatalError
 from cerbero.enums import Platform
 from cerbero.utils import _, shell
 
-NDK_VERSION = 'r25b'
+NDK_VERSION = 'r25c'
 NDK_BASE_URL = 'https://dl.google.com/android/repository/android-ndk-%s-%s.zip'
 NDK_CHECKSUMS = {
-    'android-ndk-r25b-linux.zip': '403ac3e3020dd0db63a848dcaba6ceb2603bf64de90949d5c4361f848e44b005',
+    'android-ndk-r25c-linux.zip': '769ee342ea75f80619d985c2da990c48b3d8eaf45f48783a2d48870d04b46108',
+    'android-ndk-r25c-darwin.zip': 'b01bae969a5d0bfa0da18469f650a1628dc388672f30e0ba231da5c74245bc92',
 }
 
 class AndroidBootstrapper (BootstrapperBase):
